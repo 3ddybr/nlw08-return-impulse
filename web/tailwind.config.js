6746ee0bae71387f26f4cd4432ff61b2446e1fa2
Alberto Consoli de Araujo <68253941+3ddybr@users.noreply.github.com>
2022-05-05 14:36:51 -0300

    botao enviar feed

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: '#996DFF',
           500: '#8257e6',
         },
+      },
+      borderRadius: {
+        md: '4px',
       },
     },
   },
 
   // eslint-disable-next-line no-undef
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')],
 };
