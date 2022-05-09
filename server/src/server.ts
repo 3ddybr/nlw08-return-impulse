38cdecc62462d768cb66a3d753a6127f6e62a6a7
Alberto Consoli de Araujo <68253941+3ddybr@users.noreply.github.com>
2022-05-09 14:12:23 -0300

    nodemailer

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,40 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+import { prisma } from './prisma';
 
 const app = express();
 
-app.post('/feedbacks', (req, res) => {
-  return res.send('Hello World!');
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "f3e6c1b62c5792",
+    pass: "eff401363a490e"
+  }
+});
+
+app.post('/feedbacks', async (req, res) => {
+  const {type, comment, screenshot} = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+  await transport.sendMail({
+    from: 'Equipe Feedgeth <oi@feedback.com>',
+    to: 'Alberto <3ddy.br@gmail.com>',
+    subject: 'Novo feedback',
+    html:[
+      `<p>Novo feedback do usuário ${type}</p>`,
+      `<p>Comentario: ${comment}</p>`,
+    ].join('\n')
+  })
+  return res.status(201).json({data:feedback});
 })
 
 app.listen(3333, () => {
